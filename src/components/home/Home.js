61b81ad708b214f58d1d6d293f2e93f01b5f3a19
longmondz <longcq@emotiv.com>
2017-07-06 16:18:58 +0700

    animation

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,32 +1,31 @@
 import React, {Component} from 'react';
-import { TouchableHighlight, TouchableNativeFeedback, TouchableOpacity, StyleSheet, Text, View , Button} from 'react-native';
+import { Text, View } from 'react-native';
+import Animations from './Animations'
 
-const Home = () => {
-    handlePress = () => false
+class Home extends Component {
+    // constructor(){
+    //     super()
+    //     this.updateState = this.updateState.bind(this)
+    // }
 
-    return (
-        <View style = {styles.container}>
-         <TouchableNativeFeedback>
-            <Text style = {styles.text}>
-               Button
-            </Text>
-         </TouchableNativeFeedback>
-        </View>
-    );
+    // state = { 
+    //     myState: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    // }
 
+    // updateState(){
+    //     this.setState({
+    //         myState: 'The state is updated'
+    //     });
+    // }
+
+    render() {
+        return (
+            // <View>
+            //     <PresentationalComponent myState = {this.state.myState} updateState = {this.updateState} />
+            // </View>
+            <Animations />
+        );
+    }
 }
 
 export default Home
-
-const styles = StyleSheet.create ({
-   container: {
-      alignItems: 'center',
-   },
-	
-   text: {
-      borderWidth: 1,
-      padding: 25,
-      borderColor: 'black',
-      backgroundColor: 'red'
-   }
-})
