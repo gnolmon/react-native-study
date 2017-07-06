7bffcfe7a7972b06516f26880c77ef5327867096
longmondz <longcq@emotiv.com>
2017-07-06 14:52:40 +0700

    list view

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,28 +1,29 @@
 import React, {Component} from 'react';
 import { Text, View } from 'react-native';
-import PresentationalComponent from './PresentationalComponent'
+import List from './List'
 
 class Home extends Component {
-    constructor(){
-        super()
-        this.updateState = this.updateState.bind(this)
-    }
+    // constructor(){
+    //     super()
+    //     this.updateState = this.updateState.bind(this)
+    // }
 
-    state = { 
-        myState: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-    }
+    // state = { 
+    //     myState: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    // }
 
-    updateState(){
-        this.setState({
-            myState: 'The state is updated'
-        });
-    }
+    // updateState(){
+    //     this.setState({
+    //         myState: 'The state is updated'
+    //     });
+    // }
 
     render() {
         return (
-            <View>
-                <PresentationalComponent myState = {this.state.myState} updateState = {this.updateState} />
-            </View>
+            // <View>
+            //     <PresentationalComponent myState = {this.state.myState} updateState = {this.updateState} />
+            // </View>
+            <List />
         );
     }
 }
